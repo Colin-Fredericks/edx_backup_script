2f5ace35bbd3545c597287bad7d6c9998eae1edc
Colin-Fredericks <1929508+Colin-Fredericks@users.noreply.github.com>
2022-05-04 09:56:28 -0400

    Pulling in from other repo
    Basically just copying things from the staffing script.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # edx_backup_script
- Pulls course exports from multiple courses in a row.
+
+This package pulls course exports from multiple edX courses in a row, using Selenium and a headless Chrome browser.
+
+Using this script requires a plaintext file with the URLs of all the course Export pages you'd like to access, one per line.
+
+Because course exports can range in size from a few MB to a few hundred, you should make sure you have plenty of disk space available before running this script on a large number of courses.
+
+## Chrome Web Driver
+
+This repo includes a Mac version of the [Chrome webdriver](https://chromedriver.chromium.org/), which is licensed under the a [separate set of terms](https://chromium.googlesource.com/chromium/src/+/HEAD/LICENSE). If you need a different version of the driver you'll have to replace that file (using the same name).
+
+Since chromedriver is not a signed Mac application it will throw a warning the first time you run it. Go to System Preferences --> Security and Privacy and tell it to open anyway. You should be able to
+
+## Instructions
+
+To install and use for the first time:
+
+    # clone this repo
+    $> git clone https://github.com/Colin-Fredericks/edx_backup_script.git
+
+    # create a virtualenv and activate it
+    $> python3 -m venv edxbackup
+    $> source edxbackup/bin/activate
+    (edxbackup) $>
+
+    # install requirements
+    (edxbackup) $> cd hx_util
+    (edxbackup) $> pip3 install -r requirements.txt
+
+    # install edx_backup_script
+    (edxbackup) $> pip3 install .
+
+    # to add or remove staff
+    (edxbackup) $> edx_backup_script /path/to/input/file.txt
+
+    # when done
+    (edxbackup) $> deactivate
+
+On later runs you can do a simpler version:
+
+  $> source edxstaff/bin/activate
+  (edxstaff) $> edx_backup_script /path/to/input/file.txt
+  (edxstaff) $> deactivate
+
+Run the whole process from the top if you need to reinstall (for instance, if the script and/or its requirements change).
+
+## Command-line options:
+
+* -h or --help: print the instructions and quit.
+* -v or --visible: run with a visible browser instead of a headless one.
+* -o or --output: choose a download folder.
---
+++ setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+from setuptools import setup
+from setuptools import find_packages
+
+
+project_name = "edx_backup_script"
+
+
+def get_version(*file_paths):
+    """Retrieves the version from [your_package]/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+version = get_version(project_name, "__init__.py")
+
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "async-generator",
+    "attrs",
+    "certifi",
+    "cffi",
+    "cryptography",
+    "h11",
+    "idna",
+    "outcome",
+    "pycparser",
+    "pyOpenSSL",
+    "PySocks",
+    "selenium",
+    "sniffio",
+    "sortedcontainers",
+    "trio",
+    "trio-websocket",
+    "urllib3",
+    "wsproto",
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name=project_name,
+    version=version,
+    description="Downloads edX course exports from multiple courses",
+    long_description=readme,
+    author="Colin Fredericks",
+    author_email="colin_fredericks@harvard.edu",
+    url="https://github.com/Colin_Fredericks/" + project_name,
+    packages=find_packages(exclude=["tests*"]),
+    entry_points={
+        "console_scripts": [
+            "{}={}.ReplaceEdXStaff:PullEdXBackups".format(project_name, project_name),
+        ]
+    },
+    data_files=[("webdriver", ["edx_backup_script/chromedriver"])],
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords="hx edx backup tarball " + project_name,
+    classifiers=[
+        "Development Status :: Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+    ],
+    test_suite="tests",
+    tests_require=test_requirements,
+)
