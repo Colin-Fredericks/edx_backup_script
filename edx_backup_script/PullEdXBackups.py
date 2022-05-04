732327937056d55f451abe83b8e06b2a15c60caa
Colin-Fredericks <1929508+Colin-Fredericks@users.noreply.github.com>
2022-05-04 09:58:26 -0400

    Starting from multiprocesing_test base

---
+++ edx_backup_script/PullEdXBackups.py
@@ -0,0 +1,59 @@
+import time
+import random
+import multiprocessing
+
+# Runs the loop that processes things.
+# In this example it just waits a random amount of time,
+# to simulate projects taking different amounts of time.
+def ye_function(inputs, tools):
+    # As long as there's something in the input queue,
+    while not inputs.empty():
+        # Pull an input and tool off their queues.
+        i = inputs.get()
+        t = tools.get()
+        print("Tool " + t + " starting work on " + str(i))
+        # "process" the data (just waiting)
+        time.sleep(random.randint(5, 10))
+        print("Tool " + t + " finished work on " + str(i))
+        # When the tool is ready again, put it back on its queue.
+        tools.put(t)
+    print("queue empty")
+    return True
+
+
+def run():
+    # This is our data. Could be URLs to visit.
+    input_list = multiprocessing.Queue()
+    for i in range(0, 20):
+        input_list.put("project " + str(i))
+
+    # These are tools that process the data. Could be webdrivers.
+    tool_list = multiprocessing.Queue()
+    for j in ["A", "B", "C", "D", "E"]:
+        tool_list.put("tool " + str(j))
+
+    # Spin up several processes, but not more than we have tools.
+    # Leave some CPU for other people.
+    num_processes = min(len(tool_list), multiprocessing.cpu_count() - 1)
+    processes = []
+
+    for n in range(0, num_processes):
+        # Creating processes that will run in parallel.
+        p = multiprocessing.Process(
+            target=ye_function,
+            args=(
+                input_list,
+                tool_list,
+            ),
+        )
+        # Track them so we can stop them later.
+        processes.append(p)
+        # Run the processes.
+        p.start()
+    for x in processes:
+        # Closes out the processes cleanly.
+        x.join()
+
+
+if __name__ == "__main__":
+    run()
