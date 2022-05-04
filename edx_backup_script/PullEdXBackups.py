94527eef22a0e8f76ce1cc61fc37327f3d65cbbe
Colin-Fredericks <1929508+Colin-Fredericks@users.noreply.github.com>
2022-05-04 10:07:42 -0400

    Useful stuff from staffing script

--- edx_backup_script/PullEdXBackups.py
+++ edx_backup_script/PullEdXBackups.py
@@ -1,6 +1,118 @@
+#!/usr/bin/env python3
+# VPAL Backup Script for edX Courses
+
+import os
+import csv
+import sys
 import time
 import random
+import logging
+import datetime
+import argparse
 import multiprocessing
+from getpass import getpass
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+instructions = """
+to run:
+python3 PullEdXBackups.py (options) filename.csv
+
+This script accesses multiple courses' Export pages
+to download .tar.gz files for backup purposes.
+
+The csv file only needs one header:
+URL - the address of class' Export page
+
+Options:
+-h or --help: Print this message and exit.
+-o or --output: Sets the folder where the course exports will be saved.
+                Defaults to the current directory.
+
+This script also creates a second CSV file, missing_exports.csv,
+which shows which courses couldn't be accessed.
+"""
+
+# Prep the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler("edx_backup.log")
+formatter = logging.Formatter(
+    "%(asctime)s : %(name)s  : %(funcName)s : %(levelname)s : %(message)s"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+# Just a faster thing to type and read.
+def log(text, level="INFO"):
+    print(text)
+    if level == "DEBUG":
+        logger.debug(text)
+    if level == "INFO":
+        logger.info(text)
+    if level == "WARNING":
+        logger.warning(text)
+    if level == "ERROR":
+        logger.error(text)
+    if level == "CRITICAL":
+        logger.critical(text)
+
+
+# Instantiating a headless Chrome browser
+def setUpWebdriver(run_headless):
+    log("Setting up webdriver.")
+    os.environ["PATH"] = os.environ["PATH"] + os.pathsep + os.path.dirname(__file__)
+    op = Options()
+    if run_headless:
+        op.add_argument("--headless")
+    driver = webdriver.Chrome(options=op)
+    driver.implicitly_wait(1)
+    return driver
+
+
+def signIn(driver, username, password):
+
+    # Locations
+    login_page = "https://authn.edx.org/login"
+    username_input_css = "#emailOrUsername"
+    password_input_css = "#password"
+    login_button_css = ".login-button-width"
+
+    # Open the edX sign-in page
+    log("Logging in...")
+    driver.get(login_page)
+
+    # Sign in
+    username_field = driver.find_elements(By.CSS_SELECTOR, username_input_css)[0]
+    username_field.clear()
+    username_field.send_keys(username)
+    password_field = driver.find_elements(By.CSS_SELECTOR, password_input_css)[0]
+    password_field.clear()
+    password_field.send_keys(password)
+    login_button = driver.find_elements(By.CSS_SELECTOR, login_button_css)[0]
+    login_button.click()
+
+    # Check to make sure we're signed in
+    try:
+        found_dashboard = WebDriverWait(driver, 10).until(
+            EC.title_contains("Dashboard")
+        )
+    except:
+        driver.close()
+        if "Forbidden" in driver.title:
+            sys.exit("403: Forbidden")
+        if "Login" in driver.title:
+            sys.exit("Took too long to log in.")
+        sys.exit("Could not log into edX or course dashboard page timed out.")
+
+    log("Logged in.")
+    return
+
 
 # Runs the loop that processes things.
 # In this example it just waits a random amount of time,
@@ -21,7 +133,7 @@ def ye_function(inputs, tools):
     return True
 
 
-def run():
+def PullEdXBackups():
     # This is our data. Could be URLs to visit.
     input_list = multiprocessing.Queue()
     for i in range(0, 20):
@@ -56,4 +168,4 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    PullEdXBackups()
